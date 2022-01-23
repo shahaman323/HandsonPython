baf16de39c9c189b9e3d0255a4034e3965181a12
shah323 <shahaman323@gmail.com>
2022-01-23 21:22:55 +0545

    operators in python

---
+++ calculation.py
@@ -0,0 +1,48 @@
+# adding two numbers
+print(2+2)
+
+# subtraction
+print(5-2)
+
+# multiplication
+print(2*2)
+
+# divide
+print(4/2)
+
+# to print only integer not float value use double //
+# also called integer division or floor division
+print(4//2)
+
+# python follows bodmas rule
+print(8+2*3)
+
+print((8+2)*3)
+
+# cube
+print(2*2*2)
+# what if 2 to the power 5
+# for that we can use 2 **5
+print(2**5)
+
+# modulus
+print(10%3)
+
+# string
+print('aman')
+
+print("aman's laptop")
+# what if i want to print along with ""
+# use \ to tell python to escape its general use
+# print("aman's "laptop"")
+print('aman\'s "laptop"')
+
+# print aman multiple times
+print('aman'+'aman')
+print(11*'aman')
+
+# print raw string
+# it will print umbers
+print('\home\shah\numbers')
+# use r to print raw string
+print(r'\home\shah\numbers')
