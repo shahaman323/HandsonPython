575c26f647cff70c9aebd8d3256e3c2203b5694e
shah323 <shahaman323@gmail.com>
2022-01-29 22:38:15 +0545

    set and some oprations on it

---
+++ set.py
@@ -0,0 +1,23 @@
+# There are four collection data types in the Python programming language:
+#
+#     List is a collection which is ordered and changeable. Allows duplicate members.
+#     Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+#     Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+#     Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+# set starts with {}
+# sets are collection of unique values
+# sets are unidexed and unordered
+
+# lets create a set
+
+my_set = {22,11,44,23}
+print("my_set",my_set)
+
+# set is not in sequence, it uses hash
+
+# set doesnot allow duplicate values
+my_set1={22,44,55,22,87}
+# it will print 22 at once only
+print("my_set1",my_set1)
+
